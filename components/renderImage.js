51f792ea22ae5c3f541d770479b7438b07f07eb7
Brooks Lyrette <brooks@lyrette.ca>
2019-08-31 08:36:55 -0400

    Better naming

--- components/renderImage.js
+++ components/renderImage.js
@@ -2,10 +2,10 @@
  * Such evil! This is to get around onError not firing when SSR'ed
  */
 const renderImage = (image, fallbackImage) => {
-  const onerror = `this.onerror=null;this.src=this.dataset.fallback;`
+  const onerror = `this.onerror=null;this.src=this.dataset.fallbackImage;`
   return (
     <div dangerouslySetInnerHTML={{
-      __html: `<img onError="${onerror}" data-fallback=${fallbackImage} src="${image}" />`
+      __html: `<img onError="${onerror}" data-fallback-image=${fallbackImage} src="${image}" />`
     }}>
     </div>
   )
